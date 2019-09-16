f2030c13ba5559ad600bf56a2a866c8c978e4c91
Sam <samtaylor720@gmail.com>
2019-09-16 12:11:04 -0400

    Initial

---
+++ hw1/my_name.c
@@ -0,0 +1,15 @@
+/* Project: hw1
+ * Name   : Samuel Taylor 
+ * Date   : 09/12/2019
+ * File   : my_name.c
+ * Notes  : Prints out my name Samuel Taylor
+ */
+#include <stdio.h>
+
+int main(void)
+{
+
+  printf("Samuel Taylor!\n");
+  return 0;
+
+}
