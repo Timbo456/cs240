cb776dcaa8b723937db988f296a87d0dc5c2117d
Sam <samtaylor720@gmail.com>
2019-09-17 15:57:52 -0400

    hw2

--- hw2/histo.c
+++ hw2/histo.c
@@ -2,28 +2,38 @@
 
 #define IN 1
 #define OUT 0
+#define WORDS 28
+
 
 int main(void){
 	
 	int c, state;
-	int wordlength[28];
+	int wordlength[WORDS];
 	int word = 0;
-	state = OUT;
+	
+	
+	for (int a = 0; a < WORDS; ++a){
+		wordlength[a] = 0;
+	}
 
 	while ((c = getchar()) != EOF){
 		if(c == ' ' || c == '\n' || c == '\t'){
-			state = OUT;
-			wordlength[word]++;
+			
+			++wordlength[word];
 			word = 0;
 		}
-		else if (state == OUT){
-			state = IN;
+		else {
+			
 			word = word + 1;
 		}
 		
 	}
-	for (int i = 0; i < 28; i++){
-		printf("Count %d: %d\n", i, wordlength[i]);
+	for (int i = 0; i < 28; ++i){
+		printf("%2d %4d:",i , wordlength[i]);
+		for (int b = 0; b < wordlength[i]; ++b){
+			printf("*");
+		}
+		printf("\n");
 	}
 	
 	return 0;
