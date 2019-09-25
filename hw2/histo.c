bf4c4680ff81f94e3b37d0373b97f0dfed873f12
Sam <samtaylor720@gmail.com>
2019-09-25 15:30:34 -0400

    HW2 finished historgram

--- hw2/histo.c
+++ hw2/histo.c
@@ -1,55 +1,68 @@
 #include <stdio.h>
-
-
-#define WORDS 28
-
-
-int main(void){
-	
-	int c, state;
-	int wordlength[WORDS];
-	int word = 0;
-	
-	
-	
-	for (int a = 0; a < WORDS; ++a){
-		wordlength[a] = 0;
-	}
-
-	while ((c = getchar()) != EOF){
-		if((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 
-		'Z') || (c >= 0 && c <= 9)){
-		 	word = word + 1;	
-			
-		}
-		
-		else {
-			
-			wordlength[word]++;
-			word = 0;
-			
-		}
-		
-	}
-	
-	for (int i = 0; i < WORDS; ++i){
-		printf("%2d %6d ",i , wordlength[i]);
-		if ((wordlength[i] < 4000) && (wordlength[i] > 0)){
-			printf("*");
-		}
-		else{
-			for (int b = 0; b < (wordlength[i]/4000); ++b){
-				printf("*");
-			}
-		}
-		printf("\n");
-	}
-	
-	return 0;
-
-
+#define SPACE 1
+#define NOTSPACE 0
+ 
+int main (void){
+  int lettercounter = 0;
+  int c, state;
+  int words[28];
+  char longword[28];
+  
+  state = SPACE;
+ 
+  for (int j = 0; j < 28; ++j){
+      words[j] = 0;
+      
+  }
+  while ((c = getchar()) != EOF){
+    if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || (c >= '0' && c <= '9')){
+      lettercounter = lettercounter + 1;
+      longword[lettercounter] = c;
+      state = NOTSPACE;
+ 
+ 
+      if (lettercounter == 27){
+          printf("\n");
+          for (int i = 0; i < 27; ++i){
+              printf("%c", longword[i]);
+          }
+          printf("\n\n");
+      }
+ 
+    } 
+    
+    else {  
+ 	
+      if (state == NOTSPACE){
+            words[lettercounter]++;
+            lettercounter = 0;
+            state = SPACE;
+            
+      }
+      else{
+         state = SPACE;
+         
+      }
+ 	
+    }
+ 
+  }
+  
+ 
+  for (int k = 1; k < 28; ++k){
+      printf("%2d %6d ",k , words[k]);
+      if ((words[k] < 4000) && (words[k] > 0)){
+          printf("*");
+      }
+      else{
+          for (int b = 0; b < (words[k]/4000); ++b){
+              printf("*");
+          }
+      }
+      printf("\n");
+  }
+ 
+ 
+return 0;
+ 
 }
-
-
-
-
